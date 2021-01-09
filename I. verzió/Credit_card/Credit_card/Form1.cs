bef50df9e563dee3152d631b421eace6982dfec0
keridavid1996 <76951017+keridavid1996@users.noreply.github.com>
2021-01-09 08:33:52 +0100

    I. verzió feltöltése

---
+++ I. verzió/Credit_card/Credit_card/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Credit_card
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"F:\Redwoods\PCC front.jpg");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Image = Image.FromFile(@"F:\Redwoods\PCC back.jpg");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
